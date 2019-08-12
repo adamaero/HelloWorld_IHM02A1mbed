74106bfd72b701385e6c41b8e8610ba3cf2fc1bf
Adam <adamaero@hotmail.com>
2019-08-12 12:04:12 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,106 +3,48 @@
 #include "DevSPI.h"
 #include "XNucleoIHM02A1.h"
 
-#define MPR_1 4 /* Number of movements per revolution. */
+#define STEPS_2 (200 * 128 * 2)
 
-#define STEPS_1 (200 * 128)   /* 1 revolution given a 200 steps motor configured at 1/128 microstep mode. */
-#define STEPS_2 (STEPS_1 * 2)
-
-/* Delay in milliseconds. */
-#define DELAY_1 1000
-#define DELAY_3 5000
-
-/* Motor Control Expansion Board. */
 XNucleoIHM02A1 *x_nucleo_ihm02a1;
 XNucleoIHM02A1 *x_nucleo_ihm02a1two;
 
-/* Initialization parameters of the motors connected to the expansion board. */
 L6470_init_t init[L6470DAISYCHAINSIZE] = {
-    /* First Motor. */
     {
-        24.0,                          /* Motor supply voltage in V. */
-        200,                           /* Min number of steps per revolution for the motor. */
-        1.7,                           /* Max motor phase voltage in A. */
-        3.06,                          /* Max motor phase voltage in V. */
-        300.0,                         /* Motor initial speed [step/s]. */
-        500.0,                         /* Motor acceleration [step/s^2] (comment for infinite acceleration mode). */
-        500.0,                         /* Motor deceleration [step/s^2] (comment for infinite deceleration mode). */
-        992.0,                         /* Motor maximum speed [step/s]. */
-        0.0,                           /* Motor minimum speed [step/s]. */
-        602.7,                         /* Motor full-step speed threshold [step/s]. */
-        3.06,                          /* Holding kval [V]. */
-        3.06,                          /* Constant speed kval [V]. */
-        3.06,                          /* Acceleration starting kval [V]. */
-        3.06,                          /* Deceleration starting kval [V]. */
-        61.52,                         /* Intersect speed for bemf compensation curve slope changing [step/s]. */
-        392.1569e-6,                   /* Start slope [s/step]. */
-        643.1372e-6,                   /* Acceleration final slope [s/step]. */
-        643.1372e-6,                   /* Deceleration final slope [s/step]. */
-        0,                             /* Thermal compensation factor (range [0, 15]). */
-        3.06 * 1000 * 1.10,            /* Ocd threshold [ma] (range [375 ma, 6000 ma]). */
-        3.06 * 1000 * 1.00,            /* Stall threshold [ma] (range [31.25 ma, 4000 ma]). */
-        StepperMotor::STEP_MODE_1_128, /* Step mode selection. */
-        0xFF,                          /* Alarm conditions enable. */
-        0x2E88                         /* Ic configuration. */
+                24.0,
+				200,
+				StepperMotor::STEP_MODE_1_128,
+				0xFF,
+				0x2E88
     },
-
-    /* Second Motor. */
     {
-        24.0,                           /* Motor supply voltage in V. */
-        200,                           /* Min number of steps per revolution for the motor. */
-        1.7,                           /* Max motor phase voltage in A. */
-        3.06,                          /* Max motor phase voltage in V. */
-        300.0,                         /* Motor initial speed [step/s]. */
-        500.0,                         /* Motor acceleration [step/s^2] (comment for infinite acceleration mode). */
-        500.0,                         /* Motor deceleration [step/s^2] (comment for infinite deceleration mode). */
-        992.0,                         /* Motor maximum speed [step/s]. */
-        0.0,                           /* Motor minimum speed [step/s]. */
-        602.7,                         /* Motor full-step speed threshold [step/s]. */
-        3.06,                          /* Holding kval [V]. */
-        3.06,                          /* Constant speed kval [V]. */
-        3.06,                          /* Acceleration starting kval [V]. */
-        3.06,                          /* Deceleration starting kval [V]. */
-        61.52,                         /* Intersect speed for bemf compensation curve slope changing [step/s]. */
-        392.1569e-6,                   /* Start slope [s/step]. */
-        643.1372e-6,                   /* Acceleration final slope [s/step]. */
-        643.1372e-6,                   /* Deceleration final slope [s/step]. */
-        0,                             /* Thermal compensation factor (range [0, 15]). */
-        3.06 * 1000 * 1.10,            /* Ocd threshold [ma] (range [375 ma, 6000 ma]). */
-        3.06 * 1000 * 1.00,            /* Stall threshold [ma] (range [31.25 ma, 4000 ma]). */
-        StepperMotor::STEP_MODE_1_128, /* Step mode selection. */
-        0xFF,                          /* Alarm conditions enable. */
-        0x2E88                         /* Ic configuration. */
+                24.0,
+				200,
+				StepperMotor::STEP_MODE_1_128,
+				0xFF,
+				0x2E88
     }
 };
-
 int main()
 {
-    /* Initializing SPI bus. */
 #ifdef TARGET_STM32F429
     DevSPI dev_spi(D11, D12, D13);
 #else
     DevSPI dev_spi(D11, D12, D13);
 #endif
 
-    /* Initializing Motor Control Expansion Board. */
-    x_nucleo_ihm02a1 =   newXNucleoIHM02A1(&L6470_init[ 0 ],&L6470_init[ 1 ], A4, A5, D4, A2, dev_spi);
-    x_nucleo_ihm02a1two =newXNucleoIHM02A1(&L6470_init[ 2 ],&L6470_init[ 3 ], A4, A5, D4, D2, dev_spi);
-                                        //                                      
-    /* Building a list of motor control components. */
+    x_nucleo_ihm02a1 =    new XNucleoIHM02A1(&init[0], &init[1], A4, A5, D4, D2, &dev_spi);
+    x_nucleo_ihm02a1two = new XNucleoIHM02A1(&init[0], &init[1], A4, A5, D4, A2, &dev_spi);    
+
     L6470 **motors = x_nucleo_ihm02a1->get_components();
-    L6470 **motors = x_nucleo_ihm02a1two->get_components();
+    L6470 **motorstwo = x_nucleo_ihm02a1two->get_components();
 
     /* Setting the home position. */
-    motors[3]->set_home();
-    wait_ms(DELAY_1);
-    int position = motors[3]->get_position();
-    wait_ms(DELAY_1);
-    
-    
-    motors[3]->move(StepperMotor::BWD, STEPS_2);
-    motors[3]->wait_while_active();
-    wait_ms(DELAY_1);
-    position = motors[3]->get_position();
-    wait_ms(DELAY_1);        
-    
+    //motorstwo[1]->set_home();
+    int position = motorstwo[1]->get_position();
+           
+//      motors[1]->move(StepperMotor::FWD, STEPS_2);
+//    wait_ms(DELAY_2);
+      motorstwo[1]->move(StepperMotor::FWD, STEPS_2);
+      
+    position = motorstwo[1]->get_position();
 }
