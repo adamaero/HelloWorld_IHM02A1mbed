e55623828554e22eed2f30abae27e163b4cef881
Adam <adamaero@hotmail.com>
2019-08-15 15:04:35 -0500

    Update zblink.cpp

--- zblink.cpp
+++ zblink.cpp
@@ -1,39 +1,19 @@
-/* Includes ------------------------------------------------------------------*/
-
-/* mbed specific header files. */
 #include "mbed.h"
-
-/* Helper header files. */
 #include "DevSPI.h"
-
-/* Expansion Board specific header files. */
 #include "XNucleoIHM02A1.h"
 
 ///DigitalIn User_Button(USER_BUTTON);
 
-/* Definitions ---------------------------------------------------------------*/
-
-/* Number of movements per revolution. */
 #define MPR_1 4
 
-/* Variables -----------------------------------------------------------------*/
-
-/* Motor Control Expansion Board. */
 XNucleoIHM02A1 *x_nucleo_ihm02a1;
 
-
-
 DigitalOut myled(LED1);
+DigitalOut red(LED1);
 
-
-
-* Main ----------------------------------------------------------------------*/
 
 int main()
 {
-    /*----- Initialization. -----*/
-
-    /* Initializing SPI bus. */
 #ifdef TARGET_STM32F429
     DevSPI dev_spi(D11, D12, D13);
 #else
